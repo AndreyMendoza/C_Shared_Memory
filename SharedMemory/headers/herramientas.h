a30ec690ff81dc3a427ad62984c4e6f306d99c29
Armando López C <armando12101996@gmail.com>
2017-10-22 16:50:52 -0600

    Asignación de parámetros en el main, función random, etc.

--- SharedMemory/headers/herramientas.h
+++ SharedMemory/headers/herramientas.h
@@ -1,9 +1,10 @@
 #ifndef SHAREDMEMORY_HERRAMIENTAS_H
 #define SHAREDMEMORY_HERRAMIENTAS_H
 
+#include <time.h>
 #include "imports.h"
 
-
+int random_number(int min_num, int max_num);
 void sem_lock(int sem_set_id);
 void sem_unlock(int sem_set_id);
 void save_int(int num, char * file_name);
--- SharedMemory/main.c
+++ SharedMemory/main.c
@@ -18,14 +18,54 @@ int main(int argc, char **argv) {
         inicializar(size);
     }
 
-    else if (strcmp(modo_programa, "prod") == 0)
+    else if (strcmp(modo_programa, "prod") == 0) {
+        // Tipo de algoritmo (Paginación| Segmentación)
+        // tipoAlgoritmo = "-s" | "-p";
+        //char * tipoAlgoritmo = argv[2];
+        char * tipoAlgoritmo = "-s";
+
+        //-----------Valores generados para cada hilo-----------
+        //------------------------------------------------------
+        // Variables de segmentación
+        int cant_segmentos = random_number(1,5);
+        int cant_espaciosXsegmento = random_number(1,3);
+
+        // Variables de paginación
+        int cant_paginas = random_number(1,10);
+
+        // Variable común entre paginación y segmentación
+        int tiempo = random_number(20,60);
+        //------------------------------------------------------
+        //------------------------------------------------------
+
+        // La Distribución con la que se generaran los procesos
+        // estará determinada por un tiempo aleatorio(30s-60s)
+        int distribucion_generador = random_number(30,60);
+
         producir(atoi(argv[1]));
+    }
 
-    else if (strcmp(modo_programa, "espia") == 0)
-        espiar();
+    else if (strcmp(modo_programa, "espia") == 0){
+        // Estado de la memoria -> mem
+        // Estado de los procesos -> proc
+        char * solicitud = "mem";
 
-    else if (strcmp(modo_programa, "fin") == 0)
+        if(strcmp(solicitud,"mem")==0){
+            espiarMem();
+        }
+        else if(strcmp(solicitud,"proc")==0){
+            espiarProc();
+        }
+        else{
+            printf("Solicitud %s desconocida.",solicitud);
+            printf("Comandos de espía aceptados:\n\t-> mem, para espiar la memoria\n\t-> proc, para espiar los procesos");
+        }
+    }
+
+    else if (strcmp(modo_programa, "fin") == 0) {
+
         finalizar();
+    }
 
 
     printf("\n");
